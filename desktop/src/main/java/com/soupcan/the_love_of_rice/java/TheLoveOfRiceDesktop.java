08db8363d475ccdf423443c41b4d1053fbd85044
Zachary Latta <zchlatta@gmail.com>
2013-04-13 14:38:09 -0700

    It runs on desktops!

--- desktop/src/main/java/com/soupcan/the_love_of_rice/java/TheLoveOfRiceDesktop.java
+++ desktop/src/main/java/com/soupcan/the_love_of_rice/java/TheLoveOfRiceDesktop.java
@@ -5,10 +5,12 @@ import com.soupcan.the_love_of_rice.core.TheLoveOfRice;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class TheLoveOfRiceDesktop {
-	public static void main(String[] args) {
+public class TheLoveOfRiceDesktop
+{
+	public static void main(String[] args)
+    {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = true;
-		new LwjglApplication(new TheLoveOfRice(), config);
+		new LwjglApplication(TheLoveOfRice.instance, config);
 	}
 }
