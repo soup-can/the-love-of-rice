26a43f78328f86d4b69c1cc405c64811aabf3544
Zach Latta <zchlatta@gmail.com>
2013-04-20 21:38:53 -0800

    Fix tabbing in TheLoveOfRiceDesktop.

--- desktop/src/main/java/com/soupcan/the_love_of_rice/java/TheLoveOfRiceDesktop.java
+++ desktop/src/main/java/com/soupcan/the_love_of_rice/java/TheLoveOfRiceDesktop.java
@@ -7,13 +7,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class TheLoveOfRiceDesktop
 {
-	public static void main(String[] args)
+    public static void main(String[] args)
     {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.useGL20 = true;
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.useGL20 = true;
         config.width = 1280 - 96;
         config.height = 768;
         config.title = "The Love of Rice";
-		new LwjglApplication(TheLoveOfRice.instance, config);
-	}
+        new LwjglApplication(TheLoveOfRice.instance, config);
+    }
 }
